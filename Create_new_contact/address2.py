243c6a38bc4c26614b7482969fa6285753dc11d9
Michael Krasickiy <krasickiymichael@gmail.com>
2022-02-21 17:48:10 +0300

    Save_7_21.02.2022

---
+++ Create_new_contact/address2.py
@@ -0,0 +1,5 @@
+class Address2:
+    def __init__(self, address2, phone2, notes):
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
--- contact.py
+++
@@ -1,4 +0,0 @@
-class Contact:
-    def __init__(self, firstname, middlename, ):
-        self.firstname = firstname
-        self.middlename = middlename
