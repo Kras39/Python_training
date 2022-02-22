bd45318dddd3abac67938668615405235c73015e
Michael Krasickiy <krasickiymichael@gmail.com>
2022-02-22 19:42:55 +0300

    Save_11_22.02.2022

---
+++ contacts.py
@@ -0,0 +1,9 @@
+class Contacts:
+    def __init__(self, firstname, middlename, lastname, nikename, title, company, address):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nikename = nikename
+        self.title = title
+        self.company = company
+        self.address = address
