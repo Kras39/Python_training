8f5f021d83b3cf852be59ea722d2a6e41fee450d
Michael Krasickiy <krasickiymichael@gmail.com>
2022-02-25 23:58:07 +0300

    Выделен вспомогательный класс для управления сессиями_25.02.2022

--- test/test_create_new_groups.py
+++ test/test_create_new_groups.py
@@ -13,12 +13,12 @@ def app(request):
     return fixure
 
 def test_add_groups(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="new_group", header="new_group", footer="new_group"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_groups(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
