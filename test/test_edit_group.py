3ddaae59a095707ad029b9c1b6638720ebf2606f
Krasickiy Michael <butuzz32@gmail.com>
2022-03-06 00:21:16 +0300

    Повышение "интеллектуальности" функций login и logout: проверка текущего состояния сессии_06.03.2022 Реализация проверки валидности фикстуры и проверку состояния сессии_06.03.2022

--- test/test_edit_group.py
+++
@@ -1,7 +0,0 @@
-from model.group import Group
-
-
-def test_edit_groups(app):
-    app.session.login(username="admin", password="secret")
-    app.group.edit(Group(name="Portal Group", header="PG", footer="One of the nicest groups"))
-    app.session.logout()
