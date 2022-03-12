a314acea79391e7bda75064f8549a4d12953feaa
Krasickiy Michael <butuzz32@gmail.com>
2022-03-12 20:59:26 +0300

    Доработка задания №9 (корректировка функции count в фикстуре класса contact.py)_12.03.2022

--- fixture/contact.py
+++ fixture/contact.py
@@ -90,7 +90,7 @@ class ContactHelper:
 
     def count(self):
         wd = self.app.wd
-        self.open_add_new()
+        self.app.open_home_page()
         return len(wd.find_elements_by_name("selected[]"))
 
     def add_first_contact_to_group(self):
